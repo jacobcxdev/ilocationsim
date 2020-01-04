d0e944099825d524352d415140ff6618ac9f1a00
Jacob Clayden <jacobcxdev@gmail.com>
2020-01-04 23:12:21 +0000

    Initial Commit

---
+++ ilocationsim/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ilocationsim
+//
+//  Created by Jacob Clayden on 04/01/2020.
+//  Copyright © 2020 JacobCXDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
